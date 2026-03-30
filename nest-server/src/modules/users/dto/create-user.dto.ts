a3a4bd32de6d7e0bcbe54cd7416f7c6e936d2b48
Rajat Verma <cserajatverma@gmail.com>
2026-03-30 22:44:35 +0530

    Add CreateUserDto with validation and Swagger docs
    Introduce CreateUserDto for user creation in the users module. The DTO uses class-validator decorators to enforce types and constraints (userName, firstName, lastName, email validated with IsEmail, password with MinLength(8)) and marks middleName as optional. ApiProperty/ApiPropertyOptional decorators provide OpenAPI metadata for Swagger.

---
+++ nest-server/src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,30 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty()
+  @IsString()
+  userName: string;
+
+  @ApiProperty()
+  @IsString()
+  firstName: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  middleName?: string;
+
+  @ApiProperty()
+  @IsString()
+  lastName: string;
+
+  @ApiProperty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ minLength: 8 })
+  @IsString()
+  @MinLength(8)
+  password: string;
+}
